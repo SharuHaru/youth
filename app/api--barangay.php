f40e670dc7384890014331413b73050147a2cf72
SharuHaru <harveygonzaga222@gmail.com>
2025-10-13 21:25:57 +0800

    feat(cross-site-posting): Added new table in youth database called barangay_facebook_pages

--- app/api--barangay.php
+++ app/api--barangay.php
@@ -124,7 +124,7 @@ else if($action === 'barangay-dashboard') {
     returnSuccess([
         'SkOfficialCount' => $SKOfficialsCount = SkOfficial::getPositionCount( $_POST['barangaySlug']),
         'reportAchievement' => $BarangayAchievement = Achievement::getMonthlySummary($_POST['barangaySlug']),
-        'reportAnnouncement' => $barangayAnnouncement = Announcement::getMonthlySummary($_POST['barangaySlug'], 2025)
+        'reportAnnouncement' => $barangayAnnouncement = Announcement::getMonthlySummary($_POST['barangaySlug'])
     ]);
 }
 
